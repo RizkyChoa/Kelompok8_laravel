f9118310edb20c25f7777f3c757944f98242639a
unknown <choa52.mrf@gmail.com>
2023-12-17 00:19:49 +0700

    done

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="#">Kelompok 8</a></strong>
+    <div class="float-right d-none d-sm-inline-block">
+        <b>UNIVERSITAS MUHAMMADIYAH RIAU</b> 
+    </div>
+</footer>
